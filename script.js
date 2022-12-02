4ca24ec68eb4b74bad8cb92a12549065532b8edf
Anastasiabgd <anastasia84bgd@gmail.com>
2022-12-02 14:58:17 +0400

    finished

--- script.js
+++ script.js
@@ -1,76 +1,78 @@
-let user = {
-    firstname: "giorgi",
-    lastname: "smith",
-    age: 25,
-    studentstatus: "active"
-}
+//////////////////////////HW 1
 
-console.log(user.studentstatus);
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+// }
 
-            // Or //
+// console.log(user.studentstatus);
 
-console.log(user['studentstatus']);
+//             // Or //
+
+// console.log(user['studentstatus']);
 
 
-/////////////////////////////////////////
+/////////////////////////////////////////HW 2
 
-let fruits = ['bananas', 'apple', 'pear', 'peach', 'grapes'];
+// let fruits = ['bananas', 'apple', 'pear', 'peach', 'grapes'];
 
-for(let x=0; x<5; x++) {
-    console.log(fruits[x]);
-}
-
-            //Or//
-
-for(let x=0; x<=4; x++) {
-    console.log(fruits[x]);
-}
+// for(let x=0; x<5; x++) {
+//     console.log(fruits[x]);
+// }
 
             //Or//
 
-for(let x=0; x < fruits.length; x++) {
-    console.log(fruits[x]);
-}
+// for(let x=0; x<=4; x++) {
+//     console.log(fruits[x]);
+// }
+
+            //Or//
+
+// for(let x=0; x < fruits.length; x++) {
+//     console.log(fruits[x]);
+// }
 
            //Or//
 
-for(let element of fruits) {
-    console.log(element);
-}          
+// for(let element of fruits) {
+//     console.log(element);
+// }          
 
           //Or//
 
 
-let x=0;
-while (x < fruits.length) {
-    console.log(fruits[x]);
-    x++;
+// let x=0;
+// while (x < fruits.length) {
+//     console.log(fruits[x]);
+//     x++;
     
-}          
+// }          
 
-///////////////////////////////////////////
+///////////////////////////////////////////HW 3
 
 
-let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
 
-for(let a = 0; a < numbers.length; a++) {
-if(numbers[a] > 5) {
-    console.log(numbers[a]);
-}
+// for(let a = 0; a < numbers.length; a++) {
+// if(numbers[a] > 5) {
+//     console.log(numbers[a]);
+// }
     
-}
+// }
 
                  //Or//
 
-for(let elements of numbers) {
-    if(elements > 5) {
-        console.log(elements);
-    }
-}                 
+// for(let elements of numbers) {
+//     if(elements > 5) {
+//         console.log(elements);
+//     }
+// }                 
 
 
 
-//////////////////////////////////////////////
+//////////////////////////////////////////////HW 4
 
 
 
@@ -82,13 +84,108 @@ for(let elements of numbers) {
 // }
 
 
+    // if(user.age < 18 && user.studentstatus == "active"){
+    //     console.log('Hello');
+    // }
+    // else if(user.name == 'levani'){
+    //     console.log('Hello Levani');
+    // }
+    // else if(user.studentstatus == 'active' || user.age < 25){
+    //     console.log('hello world');
+    // }
+    // else{
+    //     console.log(error);
+    // }
+    
+
+   
 
 
-//////////////////////////////////////////
+
+
+
+//////////////////////////////////////////HW 5
 
 
 
 // let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+
+// for(let z of array){
+//     if(typeof z == 'string'){
+//         console.log(z);
+//     }
+// }
+
+
+////////////////////////////////////////////HW 6
+
+
+// let array = [ [2, -3, 5, 10], [25, -24, -11, 100], [-6, -7, 10] ];
+
+
+// for(let i of array) {
+//     // console.log(i);
+//     for(let positive of i){
+//         // console.log(positive);
+
+//         if(positive > 0){
+//         console.log(positive);
+//     }
+//     }
+    
+// }
+
+
+
+
+///////////////////////////////////HW 7
+
+
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ];
+
+// for(let x=0; x < array.length; x++){
+//     if(array[x] % 2 == 0){
+//         console.log(array[x]);
+//     }
+// }
+
+//////////////// OR /////////////
+
+
+// for(let x of array){
+//     if(x % 2 == 0){
+//         console.log(x);
+//     }
+// }
+
+
+
+/////////////////////////////////////////HW 8
+
+
+// let users = [
+//     {username: 'giorgi', status: false},
+//     {username: 'levani', status: false},
+//     {username: 'anna', status: true}
+// ]
+
+// for(let b of users){
+//     if(b.status == true){
+//         console.log(b);
+//     }
+// }
+
+
+
+
+
+
+
+    
+        
+
+
+
 
  
 
